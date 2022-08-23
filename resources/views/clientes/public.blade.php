b5482979bbbc274cebacdfd658bdeafcdc96ba62
Régis Picáz Tavares <registavares@protonmail.com>
2022-08-23 01:54:28 -0300

    Ajuste no espaçamento da Logo, Alinhamento e espaçamento superior e inferior do paragrafo Descrição do Cliente, Adição de ícones redes sociais com biblioteca Font Awsome

--- resources/views/clientes/public.blade.php
+++ resources/views/clientes/public.blade.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>{{ $cliente->nome }} </title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-    <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
-    <!-- MDB -->
-    <link rel="stylesheet" href="{{ asset('public/assets/public/css/mdb.min.css') }}" />
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="{{ asset('public/assets/public/css/style.css') }}" />
-</head>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>{{ $cliente->nome }} </title>
+        <!-- Font Awesome -->
+        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" /> -->
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+        <!-- MDB -->
+        <link rel="stylesheet" href="{{asset('assets/public/css/mdb.min.css')}}" />
+        <!-- Custom styles -->
+        <link rel="stylesheet" href="{{asset('assets/public/css/style.css')}}" />
+    </head>
 
 <body>
     <!--Main Navigation-->
@@ -53,18 +53,20 @@
                     <div>
                         <h1 class="mb-3">{!! $cliente->titulo !!}</h1>
                         <h5 class="mb-4">{!! $cliente->descricao !!}</h5>
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
+                        <div class="btnLinks">
+                            <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
                             role="button" rel="nofollow" target="_blank">Fale conosco</a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
-                            role="button" rel="nofollow" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
-                            role="button" rel="nofollow" target="_blank">Fale conosco</a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
-                            role="button" rel="nofollow" target="_blank">Fale conosco</a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
-                            role="button" rel="nofollow" target="_blank">Fale conosco</a>
-                        <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->telefone }}"
-                            role="button" rel="nofollow" target="_blank">Fale conosco</a>
+                            <div class="socialButtons">
+                                <a class="mb-4" href="{{ $cliente->instagram }}"
+                                    role="button" rel="nofollow" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                                
+                                <a class="mb-4" href="{{ $cliente->facebook }}"
+                                    role="button" rel="nofollow" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                                
+                                <a class="mb-4" href="{{ $cliente->linkedin }}"
+                                    role="button" rel="nofollow" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -73,11 +75,13 @@
     </header>
     <!--Main Navigation-->
 
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/dafdf19f34.js" crossorigin="anonymous"></script>
+
     <!-- MDB -->
-    <script type="text/javascript" src="{{ asset('public/assets/public/js/mdb.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/public/js/mdb.min.js') }}" ></script>
     <!-- Custom scripts -->
-    <script type="text/javascript" src="{{ asset('public/assets/public/js/script.js') }}" />
-    </script>
+    <script type="text/javascript" src="{{ asset('assets/public/js/script.js') }}" /></script>
 </body>
 
 </html>
