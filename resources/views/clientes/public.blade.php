b2df87c2d38f47f4fe53b8bfc2f040b630880e2a
Fernando Fagonde <fernandofagonde@gmail.com>
2022-08-25 09:13:26 -0300

    base url

--- resources/views/clientes/public.blade.php
+++ resources/views/clientes/public.blade.php
@@ -12,12 +12,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
         <!--Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <base url="/">
         <!-- MDB -->
         <!--
         <link rel="stylesheet" href="{{asset('assets/public/css/mdb.min.css')}}" />-->
-        <base url="/">
         <!-- Custom styles -->
-        <link rel="stylesheet" href="assets/public/css/style.css" />
+        <link rel="stylesheet" href="{{asset('assets/public/css/style.css')}}" />
     </head>
 
 <body>
