9fafa80c75cd1edfc95c472a1d9f0043fd28f920
Fernando Fagonde <fernandofagonde@gmail.com>
2022-11-17 00:15:11 -0300

    ajustes css

--- resources/views/clientes/public.blade.php
+++ resources/views/clientes/public.blade.php
@@ -1,193 +1,113 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>{{ $cliente->nome }} </title>
-    <!-- Font Awesome -->
-    <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" /> -->
-    <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
     <!--Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <base url="/">
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="{{ asset('public/assets/public/css/style.css') }}" />
+    <style>
+        /* Set a background image by replacing the URL below */
+        body {
+            background: url("<?php echo Storage::disk('local')->url($cliente->url . '/' . $cliente->fundo); ?>") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            background-size: cover;
+            -o-background-size: cover;
+            color: <?php echo $cliente->font_color; ?>;
+        }
 
-    <!-- MDB -->
-    <!--
-        <link rel="stylesheet" href="{{ asset('public/assets/public/css/mdb.min.css') }}" />-->
+        .btn {
+            color: <?php echo $cliente->button_font_color; ?>;
+            background-color: <?php echo $cliente->button_color; ?>;
+            border: none;
+        }
+
+        .card {
+            background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2));
+        }
+
+        .socialButtons i {
+            margin-left: 0;
+            margin: 30px;
+            margin-top: 1em;
+        }
+    </style>
 </head>
 
 <body>
-    <!--Main Navigation-->
-    <header>
-        <style>
-            body {
-                background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url("<?php echo Storage::disk('local')->url($cliente->url . '/' . $cliente->fundo); ?>");
-                background-size: cover;
-                background-attachment: fixed;
-            }
+    <!-- Page Content -->
+    <div class="container">
+        <div class="card border-0 shadow my-5">
+            <div class="card-body p-5">
 
-            .mask {
-                height: 100vh;
-            }
+                <h1 class="fw-light"><img width="200"src="<?php echo Storage::disk('local')->url($cliente->url . '/' . $cliente->logo); ?>">{!! $cliente->titulo !!}</h1>
 
-            /* Espaçamento Superior e inferior da logo */
-            .container img,
-            btn {
-                margin-top: 50px;
-                margin-bottom: 80px;
-                max-width: 100%;
-            }
+                <p class="lead">
+                    {!! $cliente->descricao !!}
+                    @if (!is_null($cliente->endereco))
+                        <p> Endereço: {!! $cliente->endereco !!}</p>
+                    @endif
+                    @if (!is_null($cliente->telefone))
+                        <p> Telefone: {!! $cliente->telefone !!} </p>
+                    @endif
 
-            /*Alinhamento da descrição do cliente*/
-            p {
-                text-align: justify;
-            }
-
-            /* Espaçamento inferior do paragrafo Cliente Descrição */
-            .mb-3 {
-                margin-bottom: 2rem !important;
-            }
-
-            /* Tamanho ícones redes sociais */
-            i {
-                font-size: 2.7em !important;
-                margin-left: 1em !important;
-            }
-
-            /* Alinhamento dos botões */
-            .btnLinks {
-                margin-top: 4em;
-                padding-bottom: 2em;
-                display: flex;
-                align-items: center;
-                align-content: center;
-            }
-
-            /* Responsividade */
-
-            @media screen and (max-width: 767px) {
-
-                body {
-                    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("<?php echo Storage::disk('local')->url($cliente->url . '/' . $cliente->fundo); ?>");
-                    background-size: cover;
-                    background-attachment: fixed;
-                }
-
-                .container {
-                    text-align: center;
-                    padding: 2em;
-                }
-
-                .btnLinks {
-                    display: inline-grid;
-                }
-
-                .socialButtons i {
-                    margin-left: 0;
-                    margin: 30px;
-                    margin-top: 1em;
-                }
-            }
-
-            /* Height for devices larger than 576px */
-            @media (min-width: 992px) {
-                #intro {}
-            }
-
-            body {
-                color: {{ $cliente->font_color }};
-            }
-
-            .btn {
-                color: {{ $cliente->button_font_color }};
-                background-color: {{ $cliente->button_color }};
-                border: none;
-            }
-        </style>
-
-
-        <div id="intro" class="bg-image shadow-2-strong">
-            <div class="mask">
-                <div class="container">
-                    <img src="<?php echo Storage::disk('local')->url($cliente->url . '/' . $cliente->logo); ?>">
+                <p class="lead mb-0">
+                <div class="btnLinks row">
+                    <div class="col">
+                        @if (!is_null($cliente->whatsapp))
+                            <a class="btn btn-outline-light btn-lg m-2" href="https://wa.me/{{ $cliente->whatsapp }}"
+                                role="button" rel="nofollow" target="_blank"> <i
+                                    style="font-size:20px !important;"class="fa fa-whatsapp"></i>
+                                Chame no whatsapp</a>
+                        @endif
+                    </div>
                 </div>
-                <div class="container content">
-                    <div>
-                        <h1 class="mb-3">{!! $cliente->titulo !!}</h1>
-                        <h5 class="mb-4 col-lg-10 col-md-12">{!! $cliente->descricao !!}</h5>
-                        <div class="mb-3 col-lg-10 col-md-12">
-                            @if (!is_null($cliente->endereco))
-                                <p> Endereço: {!! $cliente->endereco !!}</p>
-                            @endif
-                            @if (!is_null($cliente->telefone))
-                                <p> Telefone: {!! $cliente->telefone !!} </p>
-                            @endif
-                        </div>
-                        <div class="btnLinks">
-                            @if (!is_null($cliente->whatsapp))
-                                <a class="btn btn-outline-light btn-lg m-2"
-                                    href="https://wa.me/{{ $cliente->whatsapp }}" role="button" rel="nofollow"
-                                    target="_blank"> <i style="font-size:20px !important;"class="fa fa-whatsapp"></i>
-                                    Chame no whatsapp</a>
-                            @endif
-                            <div class="socialButtons">
-                                @if (!is_null($cliente->instagram))
-                                    <a class="mb-4" href="https://{{ $cliente->instagram }}" role="button"
-                                        rel="nofollow" target="_blank"><i class="fa-brands fa-instagram"
-                                            style="color: {{ $cliente->button_color }}"></i></a>
-                                @endif
-                                @if (!is_null($cliente->facebook))
-                                    <a class="mb-4" href="https://{{ $cliente->facebook }}" role="button"
-                                        rel="nofollow" target="_blank"><i class="fa-brands fa-facebook"
-                                            style="color: {{ $cliente->button_color }}"></i></a>
-                                @endif
-                                @if (!is_null($cliente->linkedin))
-                                    <a class="mb-4" href="https://{{ $cliente->linkedin }}" role="button"
-                                        rel="nofollow" target="_blank"><i class="fa-brands fa-linkedin"
-                                            style="color: {{ $cliente->button_color }}"></i></a>
-                                @endif
-                                @if (!is_null($cliente->site))
-                                    <a class="mb-4" href="https://{{ $cliente->site }}" role="button" rel="nofollow"
-                                        target="_blank"><i class="fa-solid fa-globe"
-                                            style="color: {{ $cliente->button_color }}"></i></a>
-                                @endif
-                            </div>
-                        </div>
+                <div class="row">
+                    <div class="col">
+
+                        @if (!is_null($cliente->instagram))
+                            <a class="mb-4 mx-4" href="https://{{ $cliente->instagram }}"
+                                alt="perfil no Instagram"role="button" rel="nofollow" target="_blank"><i
+                                    class="fa-brands fa-instagram"
+                                    style="font-size:2em; color: {{ $cliente->button_color }}"></i></a>
+                        @endif
+                        @if (!is_null($cliente->facebook))
+                            <a class="mb-4  mx-4" href="https://{{ $cliente->facebook }}" alt="perfil no facebook"
+                                role="button" rel="nofollow" target="_blank"><i class="fa-brands fa-facebook"
+                                    style="font-size:2em; color: {{ $cliente->button_color }}"></i></a>
+                        @endif
+                        @if (!is_null($cliente->linkedin))
+                            <a class="mb-4  mx-4" href="https://{{ $cliente->linkedin }}" alt="perfil no linkedin"
+                                role="button" rel="nofollow" target="_blank"><i class="fa-brands fa-linkedin"
+                                    style="font-size:2em;; color: {{ $cliente->button_color }}"></i></a>
+                        @endif
+                        @if (!is_null($cliente->site))
+                            <a class="mb-4  mx-4" href="https://{{ $cliente->site }}" alt="site" role="button"
+                                rel="nofollow" target="_blank"><i class="fa-solid fa-globe"
+                                    style="font-size:2em; color: {{ $cliente->button_color }}"></i></a>
+                        @endif
                     </div>
                 </div>
             </div>
+            </p>
         </div>
-        <!-- Background image -->
-    </header>
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        nano® ecossistema de inovação e empreendedorismo <a class="text-white" href="https://ipsillon.cc/">Ípsillon®</a>.
     </div>
-    <!-- Copyright -->
-    <footer>
-        
-    </footer>
-    <!--Main Navigation-->
 
+    <div class="container-fluid fixed-bottom d-flex flex-wrap justify-content-end container-p-x pb-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        <div class="pt-3">            
+            <span class="footer-text ">
+                Nano - Ecossistema de Inovação e Empreendedorismo da <a class="text-white" href="https://ipsillon.cc/">
+                    <strong>Ípsillon®</strong>
+                </a>.
+                </span>
+        </div>
+    </div>
+
+    <div class=" " >
+        <h6>
+        
+           
+       
+        </h6>
+    </div>
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/dafdf19f34.js" crossorigin="anonymous"></script>
-
-    <!-- MDB -->
-    <script type="text/javascript" src="{{ asset('public/assets/public/js/mdb.min.js') }}"></script>
-    <!-- Custom scripts -->
-    <script type="text/javascript" src="{{ asset('public/assets/public/js/script.js') }}" />
-    </script>
-
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
 </body>
-
-</html>
